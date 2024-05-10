727887851ea7cd72dd467aecdd1c9dce2c8ea22f
Saurav Kharat <121320004+alpharosto@users.noreply.github.com>
2024-05-10 23:17:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 # Function to load the trained model
 @st.cache
 def load_model():
-    model_path = os.path.join("..", "models", "decision_tree_model.pkl")
+    model_path = os.path.join(./models/decision_tree_model.pkl")
 
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
